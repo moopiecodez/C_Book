99497158fda5c9014113de828d0e042e7a745d2c
Maurane van der Stoep <mauranevds@gmail.com>
2024-05-14 17:51:49 +0100

    1.3 add a header to the table

--- temperature.c
+++ temperature.c
@@ -10,6 +10,7 @@ int main() {
     step = 20;
 
     fahr = lower;
+    printf("Fahrenheit to celsius conversion:\n");
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0); 
         printf("%3.0f\t%6.1f\n", fahr, celsius); //define width of digit field
